88effcc71f4ff95028421fb046b4c0bdc6b97f89
dcvsesh <dcv.ssanchez@gmail.com>
2025-03-04 21:48:59 -0600

    Actualizacion

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
+
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
-router.get("/",(req, res)=>{
-return fs.readFile(path.join(__dirname,"../data/users.json"),
-"utf-8",
+router.get('/', (req, res) => {
+return fs.readFile(path.join(__dirname,'../data/users.json'),
+'utf-8',
 (err,data)=>{
 if(err){
   console.log(err);
@@ -17,11 +18,11 @@ res.send(users);
 );
 });
 
-router.get("/:id", (req, res)=>{
+router.get('/:id', (req, res) => {
   const params = req.params;
   const id = params.id;
-  return fs.readFile(path.join(__dirname,"../data/users.json"),
-"utf-8",
+  return fs.readFile(path.join(__dirname,'../data/users.json'),
+'utf-8',
 (err,data)=>{
 if(err){
   console.log(err);
@@ -30,12 +31,10 @@ if(err){
 const users = JSON.parse(data);
 const user = users.find(items => items._id === id)
 if(!user){
-  return res.status(404).send({message: "ID de usuario no encontrado"});
+  return res.status(404).send({message: 'ID de usuario no encontrado'});
 }
-
 res.send(user)
 }
-
 );
 })
 
