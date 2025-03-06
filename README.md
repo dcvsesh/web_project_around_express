b5a998e8651084a4854a14b972e31f0480f4886a
dcvsesh <dcv.ssanchez@gmail.com>
2025-03-05 19:13:11 -0600

    Actualizacion de ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Tripleten web_project_around_express
+
+# Proyecto Backend: Alrededor de los EE. UU.
+Este repositorio contiene el código del servidor backend para el proyecto **"Alrededor de los EE. UU."**. El objetivo de este proyecto es crear un servidor con una API que permita gestionar datos de usuarios y tarjetas, implementar autenticación y autorización, y desplegar el servidor en una máquina remota.
+
+## Descripción del Proyecto
+El proyecto consiste en desarrollar un servidor backend que soporte las siguientes funcionalidades:
+
+1. **Gestión de usuarios**:
+   - Actualización de perfiles de usuario.
+
+2. **Gestión de tarjetas**:
+   - Creación y eliminación de tarjetas.
+   - Asociación de tarjetas a usuarios.
+
+3. **Seguridad**:
+   - Validación de datos y manejo de errores.
+
--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
+const usersRouter = require("./routes/users");
+const cardsRouter = require("./routes/cards");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
+app.use("/users", usersRouter);
+app.use("/cards", cardsRouter);
 
-app.get('*', (req, res) => {
-  res.status(404).send({ message: 'Recurso solicitado no encontrado' });
+app.get("*", (req, res) => {
+  res.status(404).send({ message: "Recurso solicitado no encontrado" });
 });
 
 app.listen(PORT, () => {
-  console.log('Servidor corriendo en puerto 3000');
+  console.log("Servidor corriendo en puerto 3000");
 });
